e25f389066565d7ee4e3f5493dbb88bc53bc8fbc
Romanuch <generalRDR@gmail.com>
2020-09-08 17:23:44 +0300

    added fonts and mixins

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-import "./sass/main.scss";
+import './sass/main.scss';
+
+// const randomTextSection = document.querySelector('.comments');
+// const commentTextContent = document.querySelector('.comments__content');
+
+// randomTextSection.addEventListener('click', getText);
+// getText();
+
+// async function getText() {
+//   const textCall = await fetch('https://icanhazdadjoke.com/', {
+//     headers: {
+//       Accept: 'application/json',
+//     },
+//   });
+//   const text = await textCall.json();
+//   console.log(text);
+// }
