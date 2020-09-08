4f08fb50a38a49c18206b9f3463c7d5ce64a56ef
MellYu <mel6273569@gmail.com>
2020-09-08 23:12:32 +0300

    ade fetch for 5 days

--- src/js/input.js
+++ src/js/input.js
@@ -1,19 +1,24 @@
 import refs from './refs.js';
-import "./fetchWeatherData.js";
+import './fetchWeatherData.js';
+import fiveDaysTemp from './fetchWeatherDataWeek.js';
+import currentDaysTemp from './../template/fiveDays.hbs';
 
 import forecastData from './fetchWeatherData.js';
 
-refs.inputRef.addEventListener('submit', e =>{
-    e.preventDefault();
-    const searchValue = e.currentTarget.elements.search.value;
-   forecastData.getForecast(searchValue).then(city => {
-        forecastData.request = searchValue;
-        console.log(city);
-        return city;
-        // refs.bookmarkRef.insertAdjacentHTML('beforeend', `<li><button>${city.name}</button></li>`);
-    })
+refs.inputRef.addEventListener('submit', e => {
+  e.preventDefault();
+  const searchValue = e.currentTarget.elements.search.value;
+  forecastData.getForecast(searchValue).then(city => {
+    // const date = new Date(city.dt*1000);
+    console.log(city);
+  });
 });
 
+console.log(
+  fiveDaysTemp.getForecast('london').then(data => {
+    const array = data.list;
+    const newArr = [];
 
-
-
+    console.log(newArr);
+  }),
+);
--- src/js/refs.js
+++ src/js/refs.js
@@ -2,4 +2,5 @@ export default {
     inputRef: document.querySelector('.main__form'),
     bookmarkRef: document.querySelector('.main__bookmarks'),
     bookmarkBtnRef: document.querySelector('.main__bookmarkBtn'),
+    fiveDaysRef: document.querySelector('.five__days'),
 }
