c30aec35d90fd2467dd0bc59371c296296e9c226
petyhov <petyhov91@gmail.com>
2020-09-09 14:21:30 +0300

    fix temperture and add lite first section

--- src/js/fetchWeatherData.js
+++ src/js/fetchWeatherData.js
@@ -1,21 +1,22 @@
 export default {
-    keyApi: '4155ac6d3fb539e9f841172aed94674f',
-    searchRequest: '',
-  
-    getForecast(request) {
-      const baseUrl = 'http://api.openweathermap.org/data/2.5/weather';
-      const params = `?q=${request}&appid=${this.keyApi}`;
-      return fetch(baseUrl + params)
-        .then(resp => resp.json())
-        .then(data => data)
-        .catch(err => err)
-    },
-  
-    // get request() {
-    //   return this.searchRequest;
-    // },
-  
-    // set request(value) {
-    //   return (this.searchRequest = value);
-    // },
-  };
+  keyApi: '4155ac6d3fb539e9f841172aed94674f',
+  searchRequest: '',
+
+  getForecast(request) {
+    const baseUrl = 'http://api.openweathermap.org/data/2.5/weather';
+    const params = `?q=${request}&appid=${this.keyApi}`;
+    const metric = `&units=metric`;
+    return fetch(baseUrl + params + metric)
+      .then(resp => resp.json())
+      .then(data => data)
+      .catch(err => err);
+  },
+
+  // get request() {
+  //   return this.searchRequest;
+  // },
+
+  // set request(value) {
+  //   return (this.searchRequest = value);
+  // },
+};
