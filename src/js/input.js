1d090f1ce2fda7259dc15f1b1c052a9423be2579
petyhov <petyhov91@gmail.com>
2020-09-10 05:18:07 +0300

    add today temperature section

--- src/js/input.js
+++ src/js/input.js
@@ -3,13 +3,15 @@ import './fetchWeatherData.js';
 import forecastData from './fetchWeatherData.js';
 import forecastForFiveDays from './fetchWeatherDataWeek.js';
 import dateBlock from './createDateBlock.js';
-import {groupByDate} from './groupByDateFunction.js';
+import { groupByDate } from './groupByDateFunction.js';
+import oneDayTemplate from './oneDayTemplate';
 
 //Блок з датою, світанком та заходом сонця
 
 refs.inputRef.addEventListener('submit', e => {
   e.preventDefault();
   const searchValue = e.currentTarget.elements.search.value;
+  oneDayTemplate(searchValue);
   // forecastData.getForecast(searchValue).then(city => {
   //   forecastData.request = searchValue;
   //   dateBlock(city);
@@ -18,38 +20,38 @@ refs.inputRef.addEventListener('submit', e => {
 
 // Блок з прогнозом погоди на 5 днів
 
-forecastForFiveDays.getForecastFiveDays('london').then(forecast =>{
+forecastForFiveDays.getForecastFiveDays('london').then(forecast => {
   const arrData = forecast.list;
-const newArr = groupByDate(arrData);
-newArr.length = 5;
-console.log(newArr);
+  const newArr = groupByDate(arrData);
+  newArr.length = 5;
+  console.log(newArr);
 
-newArr.map(el => {
-  let value;
-  for (value of el){
-    Object.values(...newArr[0])[1];
-  }
-
-const dateOfFiveDays = {
-    day: new Date(value.dt*1000).toLocaleString('en', {weekday: 'long'}),
-    date: new Date(value.dt*1000).getDate(),
-    weather: value.weather[0].icon,
-    minTemperature: value.main.temp_min,
-    maxTemperature: value.main.temp_max,
-    forecast: [{
-      time: new Date(value.dt*1000).getTime(),
-      // weather: string,
-      // pressure: num,
-      // humidity: num,
-      // wind: num
-    }]
+  newArr.map(el => {
+    let value;
+    for (value of el) {
+      Object.values(...newArr[0])[1];
     }
-console.table(dateOfFiveDays);
-console.table(dateOfFiveDays.forecast);
 
-// console.log(values);
-// console.log(minTempArr.push(values));
-});
+    const dateOfFiveDays = {
+      day: new Date(value.dt * 1000).toLocaleString('en', { weekday: 'long' }),
+      date: new Date(value.dt * 1000).getDate(),
+      weather: value.weather[0].icon,
+      minTemperature: value.main.temp_min,
+      maxTemperature: value.main.temp_max,
+      forecast: [
+        {
+          time: new Date(value.dt * 1000).getTime(),
+          // weather: string,
+          // pressure: num,
+          // humidity: num,
+          // wind: num
+        },
+      ],
+    };
+    console.table(dateOfFiveDays);
+    console.table(dateOfFiveDays.forecast);
 
-
+    // console.log(values);
+    // console.log(minTempArr.push(values));
+  });
 });
--- src/js/refs.js
+++ src/js/refs.js
@@ -4,4 +4,10 @@ export default {
   bookmarkBtnRef: document.querySelector('.main__bookmarkBtn'),
   dataBlockRef: document.querySelector('.data'),
   fiveDaysRef: document.querySelector('.five__days'),
+  oneDaySection: document.querySelector('.one-day-section'),
+  locationName: document.getElementById('locationName'),
+  locationWeather: document.getElementById('locationWeather'),
+  todayTemperature: document.getElementById('todayTemperature'),
+  todayMinTemperature: document.getElementById('todayMinTemperature'),
+  todayMaxTemperature: document.getElementById('todayMaxTemperature'),
 };
